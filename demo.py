5a21214e3e2410e45cc81828734995b8db4ac475
Desert Fox <tramphan2294@gmail.com>
2024-01-26 10:53:25 +0700

    update demo.py

--- demo.py
+++ demo.py
@@ -34,9 +34,6 @@ def process_image(in_img, out_img, show):
         
         check_intercept = intercept.detect(hlane_det)
         max_lines = check_intercept[1]['max_points']
-        if show:
-            cv2.imshow('raw image', lane_det)
-            cv2.imshow('intersection', im)
         cv2.imwrite(os.path.join(out_img, path), im)
         
         if check_intercept[0][1]<= check_thresh['gap_thresh']:
@@ -45,9 +42,57 @@ def process_image(in_img, out_img, show):
                 for i in max_lines:
                     cv2.circle(im, (i[0], i[1] + crop_height_value), 2, (0, 0, 255), -1)
                 cv2.imwrite(os.path.join(out_img, path), im)
+                
+        if show:
+            cv2.imshow('raw image', lane_det)
+            cv2.imshow('intersection', im)
 
-def process_video():
-    pass
+def process_video(in_vid, out_dir, show = True):
+    opt = action.load_config_file("main_rc.json")
+    im_pros = ImagePreprocessing.ImagePreprocessing(opt)
+    intercept = InterceptDetection.InterceptDetection(opt, debug=True)
+    
+    vid = cv2.VideoCapture(in_vid)
+    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    cnt = 0
+    while True:
+        ret, frame = vid.read()
+        frame = cv2.resize(frame, (640, 480))
+        if not ret:
+            print("Failed to capture frame")
+            break
+        check_thresh = opt['INTERCEPT_DETECTION']
+        crop_ratio = float(check_thresh['crop_ratio'])
+        height = frame.shape[0]
+        crop_height_value =  int(height * crop_ratio)
+        
+        frame_cut = frame[crop_height_value:, :]
+        lane_det = im_pros.process_image(frame)
+        hlane_det = im_pros.process_image2(frame_cut)
+        check_intercept = intercept.detect(hlane_det)
+        
+        max_lines = check_intercept[1]['max_points']
+        if check_intercept[0][1]<= check_thresh['gap_thresh']:
+            if check_intercept[0][0]>= check_thresh['max_points_thresh']:
+                for i in max_lines:
+                    cv2.circle(frame, (i[0], i[1] + crop_height_value), 2, (0, 0, 255), -1)
+        #         cnt += 1
+        # if cnt == 5:
+        #     for path in os.listdir(out_dir):
+        #         cv2.imwrite(os.path.join(out_dir, path), frame)
+        #     cnt = 0
+        if show:
+            cv2.imshow('Raw Video', cv2.resize(frame, (512, 384)))
+            cv2.imshow('Road Detection', cv2.resize(lane_det, (512, 384)))
+            cv2.imshow('Intersection', hlane_det)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     process_image(in_img=r'test_real', out_img = r'save_im3', show = False)
+    # process_video(in_vid=r'test_vid/crossroad1.mp4', out_dir=r'save_im2')
